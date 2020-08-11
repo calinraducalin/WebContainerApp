6dcefaad65481485845a76c6236623eaba19416b
Calin Radu Calin <crcalin@gmail.com>
2020-08-11 09:38:20 +0300

    Initial Commit

---
+++ WebContainerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebContainerApp
+//
+//  Created by Calin Radu Calin on 11/08/2020.
+//  Copyright © 2020 Yummly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
