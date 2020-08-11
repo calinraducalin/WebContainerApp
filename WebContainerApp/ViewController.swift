c7f5d339b7ba622f4552c29868ccb3e9f41d14ec
Calin Radu Calin <crcalin@gmail.com>
2020-08-11 10:02:51 +0300

    Added In-App Browser

--- WebContainerApp/ViewController.swift
+++ WebContainerApp/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
+
+  @IBOutlet private weak var textView: UITextView!
+  private let defaultURLString = "google.com"
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    textView.text = defaultURLString
+  }
+
+  @IBAction private func openURLButtonAction(_ sender: Any) {
+    guard var urlComponents = URLComponents(string: textView.text) else { return }
+
+    if urlComponents.scheme == nil {
+      urlComponents.scheme = "https"
+    }
+
+    openURL(urlComponents.url)
   }
 
 
+  private func openURL(_ url: URL?) {
+    guard let url = url else { return }
+    let config = SFSafariViewController.Configuration()
+    config.entersReaderIfAvailable = true
+
+    let vc = SFSafariViewController(url: url, configuration: config)
+    present(vc, animated: true)
+  }
 }
 
